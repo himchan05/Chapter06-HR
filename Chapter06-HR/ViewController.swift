632acc7f2a4dd470743fe8e3fd646a2f21bf78ae
박힘찬 <himchan05@naver.com>
2022-02-18 13:06:24 +0900

    Initial Commit

---
+++ Chapter06-HR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter06-HR
+//
+//  Created by 박힘찬 on 2022/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Chapter06-HRUITests/Chapter06_HRUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chapter06_HRUITestsLaunchTests.swift
+//  Chapter06-HRUITests
+//
+//  Created by 박힘찬 on 2022/02/18.
+//
+
+import XCTest
+
+class Chapter06_HRUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
